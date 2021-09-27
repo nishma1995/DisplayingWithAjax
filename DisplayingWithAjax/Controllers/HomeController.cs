2676587b1d004e53cbdf0dfe6a27a9a5f37630d0
USER <USER@DESKTOP-LHCIT1T>
2021-09-27 09:29:17 -0700

    Add project files.

---
+++ DisplayingWithAjax/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using DisplayingWithAjax.Models;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DisplayingWithAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult GetData()
+        {
+            using (EmployeeOperationManagementSystemEntities db = new EmployeeOperationManagementSystemEntities())
+           
+
+            {
+                List<Employee> employeeList = db.Employees.ToList<Employee>();
+                return Json(new { data = employeeList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
